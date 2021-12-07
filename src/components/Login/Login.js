a18c447f3cfbf69c86c8c0e529e8daff4ab7904e
monjur15 <monjur.git@gmail.com>
2021-12-07 22:29:42 +0600

    Login page done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee, faEnvelope, faEye } from '@fortawesome/free-solid-svg-icons';
+import { faCoffee, faEnvelope, faEye, faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 import './Login.css'
 
 
 const Login = () => {
     const emailIcon = <FontAwesomeIcon icon={faEnvelope} />;
     const passIcon = <FontAwesomeIcon icon={faEye} />;
+    const arrowIcon = <FontAwesomeIcon icon={faArrowAltCircleRight} />;
     return (
         <div className="login_from">
             <div className="login_part">
                 <div className="log_box1">
                     <div className="login_cover">
                         <img src="../../images/Training logo.png" alt=""></img>
+                        <h1>Welcome back,</h1>
                     </div>
+
                     <fieldset className="login_fields">
                         <legend>Email</legend>
 
@@ -21,17 +24,28 @@ const Login = () => {
                         <span>{emailIcon}</span>
                     </fieldset>
                 </div>
-                <div>
+                <div className="log_box2">
 
                     <fieldset className="login_fields">
                         <legend>Password</legend>
                         <input type="password" placeholder="Enter Password"></input>
                         <span>{passIcon}</span>
                     </fieldset>
+                    <small><a href="#">Forget Password</a></small>
+                </div>
+
+                <div className="button_div">
+                    <button className="blue_btn">Login</button>
+                    <h3>or</h3>
+                    <a href="#"><img src="../../images/search.png" alt="" /></a>
+                    <a href="#"><img src="../../images/facebook.png" alt="" /></a>
+                    <a href="#"><h3>Create Account {arrowIcon}</h3></a>
+
                 </div>
             </div>
             <div className="login_banner">
-                <h1>hello</h1>
+
+                <img src="../../images/bg1.png" alt="" />
             </div>
 
         </div>
