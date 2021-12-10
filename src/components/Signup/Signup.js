5ca51924d564bb10d9a7d2ef774b2d5eedf99c1f
monjur15 <monjur.git@gmail.com>
2021-12-10 16:09:32 +0600

    Signup page done

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee, faEnvelope, faEye, faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faEnvelope, faLock, faPhone } from '@fortawesome/free-solid-svg-icons';
 import './Signup.css'
 
 const Signup = () => {
+    const userIcon = <FontAwesomeIcon icon={faUser} />;
     const emailIcon = <FontAwesomeIcon icon={faEnvelope} />;
+    const passwordIcon = <FontAwesomeIcon icon={faLock} />;
+    const phoneIcon = <FontAwesomeIcon icon={faPhone} />;
     return (
         <div className='signup_main'>
             <div className="signup_banner">
@@ -25,25 +28,32 @@ const Signup = () => {
                 <h1>Create Account</h1>
                 <div className='signup_fields'>
                     <input type="text" placeholder='Enter Username' />
+                    <span>{userIcon}</span>
+
+                </div>
+
+                <div className='signup_fields'>
+                    <input type="email" placeholder='Enter Email Address' />
                     <span>{emailIcon}</span>
 
                 </div>
 
                 <div className='signup_fields'>
-                    <input type="text" placeholder='Enter Username' />
-                    <span>{emailIcon}</span>
+                    <input type="password" placeholder='Enter Password' />
+                    <span>{passwordIcon}</span>
 
                 </div>
 
                 <div className='signup_fields'>
-                    <input type="text" placeholder='Enter Username' />
-                    <span>{emailIcon}</span>
+                    <input type="tel" placeholder='Enter Contact Number' />
+                    <span>{phoneIcon}</span>
 
                 </div>
 
-                <div className='signup_fields'>
-                    <input type="text" placeholder='Enter Username' />
-                    <span>{emailIcon}</span>
+                <div className='sign_up'>
+                    <button className="blue_btn">Sign Up</button>
+                    <a href="#"><h3>Login</h3></a>
+                    <div className='bottom_bor'></div>
 
                 </div>
 
